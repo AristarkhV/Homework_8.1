a7c7ceb96a1a7de9c98190085fde5af4cf391702
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-06-10 13:04:35 +0300

    update

--- src/mateacademy/homework/homework_8/Main.java
+++ src/mateacademy/homework/homework_8/Main.java
@@ -3,6 +3,31 @@ package mateacademy.homework.homework_8;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MyArrayList<String> temp = new MyArrayList();
+        temp.add("one");
+        temp.add("two");
+        temp.add("three");
+        System.out.println("\n add test:\n");
+        for (int i = 0; i < temp.size(); i++) {
+            System.out.println(temp.get(i));
+        }
+        MyArrayList<String> str = new MyArrayList();
+        str.add("four");
+        str.add("five");
+        str.add("six");
+        str.add("seven");
+        temp.addAll(str);
+        System.out.println("\n addAll test:\n");
+        for (int i = 0; i < temp.size(); i++) {
+            System.out.println(temp.get(i));
+        }
+        System.out.println("\n size = " + temp.size());
+        System.out.println("\n remove test [five]: \n");
+        temp.remove("five");
+        for (int i = 0; i < temp.size(); i++) {
+            System.out.println(temp.get(i));
+        }
+        System.out.println("\n size = " + temp.size());
     }
 }
---
+++ src/mateacademy/homework/homework_8/MyArrayList.java
@@ -0,0 +1,104 @@
+package mateacademy.homework.homework_8;
+
+import java.util.Arrays;
+
+public class MyArrayList<T> implements List<T> {
+
+    private final static int CAPACITY = 10;
+    private T[] values;
+    private int size;
+    private int currentCapacity;
+
+    public MyArrayList() {
+        values = (T[]) new Object[CAPACITY];
+    }
+
+    private boolean indexOutOfBounds(int index) {
+        if (index < 0 || index > size()) {
+            throw new ArrayIndexOutOfBoundsException("out of bounds");
+        }
+        return true;
+    }
+
+    private void increaseCapacity() {
+        if (size > values.length) {
+            currentCapacity = currentCapacity * 3 / 2;
+        }
+    }
+
+    @Override
+    public void add(T value) {
+        increaseCapacity();
+        values = Arrays.copyOf(values, currentCapacity);
+        values[size] = value;
+        size++;
+    }
+
+    @Override
+    public void add(T value, int index) {
+        if (indexOutOfBounds(index)) {
+            increaseCapacity();
+            values = Arrays.copyOf(values, currentCapacity);
+            values[index] = value;
+            System.arraycopy(values, 0, values, index + 1, size);
+        }
+        size++;
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        increaseCapacity();
+        values = Arrays.copyOf(values, currentCapacity);
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (indexOutOfBounds(index)) {
+            return values[index];
+        }
+        return null;
+    }
+
+    @Override
+    public void set(T value, int index) {
+        if (indexOutOfBounds(index)) {
+            this.values[index] = value;
+        }
+    }
+
+    @Override
+    public T remove(int index) {
+        if (indexOutOfBounds(index)) {
+            T temp = values[index];
+            System.arraycopy(values, index + 1, values, index, values.length - index - 1);
+            values = Arrays.copyOf(values, values.length - 1);
+            size--;
+            return temp;
+        }
+        return null;
+    }
+
+    @Override
+    public T remove(T t) {
+        for (int i = 0; i < size; i++) {
+            if (values[i].equals(t)) {
+                remove(i);
+                return t;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+}
